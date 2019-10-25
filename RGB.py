3a05d3564e97e96b310e2f1c5c55443a29082748
Olivia S Pemberton <opember44@ccs.local>
2019-10-25 09:26:07 -0400

    adding last five files

---
+++ RGB.py
@@ -0,0 +1,37 @@
+from pulseio import PWMOut #pylint: disable-msg=import-error
+# import the PWMOut class from the pulseio module
+class RGB:
+    def __init__ (self, Rpin, Gpin, Bpin):
+    # the object (RGB LED) is initialized with a a red, green, and blue pins
+        self.Rpin = PWMOut (Rpin, duty_cycle = 65535, frequency = 500)
+        self.Gpin = PWMOut (Gpin, duty_cycle = 65535, frequency = 500)
+        self.Bpin = PWMOut (Bpin, duty_cycle = 65535, frequency = 500)
+        # each pin is a PWM pin, and they all start off
+    def red (self):
+        self.Rpin.duty_cycle = 0
+        self.Gpin.duty_cycle = 65535
+        self.Bpin.duty_cycle = 65535
+        # when there is power, the pin's defualt value is on, so turning them to 65535 turns them off, not on
+    def blue (self):
+        self.Rpin.duty_cycle = 65535
+        self.Gpin.duty_cycle = 65535
+        self.Bpin.duty_cycle = 0
+    def green (self):
+        self.Rpin.duty_cycle = 65535
+        self.Gpin.duty_cycle = 0
+        self.Bpin.duty_cycle = 65535
+    def cyan (self):
+        # green + blue
+        self.Rpin.duty_cycle = 65535
+        self.Gpin.duty_cycle = 0
+        self.Bpin.duty_cycle = 0
+    def magenta (self):
+        # red + blue
+        self.Rpin.duty_cycle = 0
+        self.Gpin.duty_cycle = 65535
+        self.Bpin.duty_cycle = 0
+    def yellow (self):
+        # red + green
+        self.Rpin.duty_cycle = 0
+        self.Gpin.duty_cycle = 0
+        self.Bpin.duty_cycle = 65535
---
+++ main_fancyLED.py
@@ -0,0 +1,11 @@
+from fancyLED import FancyLED
+import board
+
+fancy1 = FancyLED(board.D2,board.D3,board.D4)
+fancy2 = FancyLED(board.D5,board.D6,board.D7)
+
+while True:
+    fancy1.alternate()
+    fancy2.blink()
+    fancy1.chase()
+    fancy2.sparkle()
